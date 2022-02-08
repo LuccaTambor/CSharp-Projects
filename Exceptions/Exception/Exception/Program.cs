8261da0690072aa54c12e22ebd1f754162a8d273
Lucca Augusto Torres Tambor <lucca.tambor@outlook.com>
2022-02-08 10:16:04 -0300

    Adding a Execption example project

---
+++ Exceptions/Exception/Exception/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Exceptions.Entities;
+using Exceptions.Entities.Exceptions;
+
+namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.Write("Room Number: ");
+                int roomNumber = int.Parse(Console.ReadLine());
+                Console.Write("Check-in Data (dd/MM/yyyy): ");
+                DateTime checkin = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out Data (dd/MM/yyyy): ");
+                DateTime checkout = DateTime.Parse(Console.ReadLine());
+
+
+                Reservation reservation = new Reservation(roomNumber, checkin, checkout);
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation: ");
+                Console.Write("Check-in Data (dd/MM/yyyy): ");
+                checkin = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out Data (dd/MM/yyyy): ");
+                checkout = DateTime.Parse(Console.ReadLine());
+
+                DateTime now = DateTime.Now;
+
+                reservation.UpdateDates(checkin, checkout);
+                Console.WriteLine("Reservation: " + reservation);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error in reservation: "+ e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Error in format: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpect Error: " + e.Message);
+            }
+       
+        }
+    }
+}
+ 
